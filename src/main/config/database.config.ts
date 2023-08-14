0f9099a8de8023a80cf442534965d22baa3467be
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-14 00:39:59 -0300

    database config e connection movidas para a main

--- src/main/config/database.config.ts
+++ src/main/config/database.config.ts
@@ -1,9 +1,9 @@
 import * as dotenv from "dotenv";
 import { DataSource } from "typeorm";
-import { UserEntity } from "../entities/user.entity";
-import { ErrandEntity } from "../entities/errand.entity";
-import { CreateTableErrands1690248382540, CreateTableUsers1690248382540 } from "../migrations/1690248382540-CreateTable";
-import { AlterTable1690354320225 } from "../migrations/1690354320225-AlterTable";
+import { UserEntity } from "../../app/shared/database/entities/user.entity";
+import { ErrandEntity } from "../../app/shared/database/entities/errand.entity";
+import { CreateTableErrands1690248382540, CreateTableUsers1690248382540 } from "../../app/shared/database/migrations/1690248382540-CreateTable";
+import { AlterTable1690354320225 } from "../../app/shared/database/migrations/1690354320225-AlterTable";
 
 
 dotenv.config();
