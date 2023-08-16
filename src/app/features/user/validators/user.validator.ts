5b09ab8d338ca24cdf0b0471ab70182487aced8c
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-16 02:15:05 -0300

    validador de login e criação de usuários

--- src/app/features/user/validators/user.validator.ts
+++ src/app/features/user/validators/user.validator.ts
@@ -1,30 +1,69 @@
 import { NextFunction, Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
 import { UserRepository } from "../repositories/user.repository";
+import { JwtService } from "../../../shared/services/jwt.service";
+import { HttpResponse } from "../../../shared/util/http-response.adapter";
 
 
-export class UserMiddleware {
-    public static validateUserExists(
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) {
-        try {
-            const { userId } = req.params;
+export class UserValidator {
 
-            const user = new UserRepository().get(userId);
-            if (!user) {
-                return res
-                  .status(StatusCodes.NOT_FOUND)
-                  .send({ ok: false, message: "User was not found" });
-              }
+  public static loginValidator(req: Request, res: Response, next: NextFunction) {
+      try {
+      
+          const token = req.headers.authorization;
 
-            next();
-        } catch (error: any) {
-          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-            ok: false,
-            message: error.toString(),
+          if (!token) {
+              return HttpResponse.unauthorized(res);
+          }
+
+          const jwtService = new JwtService();
+          const isValid = jwtService.verifyToken(token);
+
+          if (!isValid) {
+              return HttpResponse.unauthorized(res);
+          }
+
+          next();
+
+      } catch (error: any) {
+          return res.status(500).send({
+              ok: false,
+              error: error.toString(),
           });
+      }
+  }
+
+  public static async createUserValidator(req: Request, res: Response, next: NextFunction){
+    try{
+        const {name, email, password} = req.body;
+
+        if(!name){
+          return HttpResponse.fieldNotProvided(res, name);
         }
-    }
+
+        if(!email){
+          return HttpResponse.fieldNotProvided(res, email);
+        }
+
+        if(!password){
+          return HttpResponse.fieldNotProvided(res, password);
+        }
+
+        const repository = new UserRepository();
+    
+        const emailValid = await repository.getByEmail(email);
+  
+        if (emailValid) {
+          return HttpResponse.unauthorized(res)
+        }
+
+        next();
+
+    } catch (error: any) {
+        return res.status(500).send({
+            ok: false,
+            error: error.toString(),
+        });
+      }
+  }
+
 }
