bb1cc6446e9f0897edf8b88bdc5f2b146e85786a
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-17 23:08:33 -0300

    feat: update-errand-usecase

---
+++ src/app/features/errand/usecases/update-errand-usecase.ts
@@ -0,0 +1,54 @@
+import { Errand } from "../../../models/errand.model";
+import { CacheRepository } from "../../../shared/database/repositories/cache.repository";
+import { UsecaseResponse } from "../../../shared/util/response.adapter";
+import { Result } from "../../../shared/util/result.contract";
+import { Usecase } from "../../../shared/util/usecase.contract";
+import { UserRepository } from "../../user/repositories/user.repository";
+import { ErrandRepository } from "../repositories/errand.repository";
+
+interface UpdateErrandParams {
+  errandId: string;
+  userId: string;
+  description: string;
+  detail: string;
+}
+
+export class updateErrandUsecase implements Usecase {
+    public async execute (params: UpdateErrandParams): Promise<Result> {
+        const repository = new UserRepository();
+        const user = await repository.get(params.userId);
+
+        if(!user){
+            return UsecaseResponse.notFound("User")
+        }
+
+        const errandRepository = new ErrandRepository();
+        const errand = await errandRepository.get({errandId: params.errandId});
+
+        if(!errand){
+            return UsecaseResponse.notFound("Errand");
+        }
+
+        if (params.description) {
+            errand.description = params.description;
+        }
+      
+        if (params.detail) {
+            errand.detail = params.detail;
+        }
+      
+        await errandRepository.update(errand);
+
+        const cacheRepository = new CacheRepository();
+        await cacheRepository.delete("errands");
+
+        const result = await new ErrandRepository().list({userId: params.userId});
+
+        const data = result.map((errand) => errand.toJson());
+
+        return UsecaseResponse.success(
+            "Errand successfully deleted",
+            data
+        )
+    }
+}
