d6465b9a2395022233a06adc29d5afd616a8a3c1
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-19 03:41:05 -0300

    refactor: archive e unarchive errands refatorados para usecase

--- src/app/features/errand/controllers/errand.controller.ts
+++ src/app/features/errand/controllers/errand.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { UserRepository } from "../../user/repositories/user.repository";
-import { ErrandRepository } from "../repositories/errand.repository";
 import { createErrandUsecase } from "../usecases/create-errand.usecase";
 import { listErrandUsecase } from "../usecases/list-errand-usecase";
 import { updateErrandUsecase } from "../usecases/update-errand-usecase";
 import { deleteErrandUsecase } from "../usecases/delete-errand-usecase";
+import { archiveErrandUsecase } from "../usecases/archive-errand-usecase";
+import { unarchiveErrandUsecase } from "../usecases/unarchive-errand-usecase";
 
 
 
@@ -93,34 +93,13 @@ export class ErrandController {
       try {
         const { userId, errandId } = req.params;
     
-        const user = await new UserRepository().get(userId);
-        if (!user) {
-          return res
-            .status(StatusCodes.NOT_FOUND)
-            .send({ ok: false, message: "User was not found" });
-        }
-    
-        const errandRepository = new ErrandRepository();
-        const errand = await errandRepository.get({ errandId });
-    
-        if (!errand) {
-          return res
-            .status(StatusCodes.NOT_FOUND)
-            .send({ ok: false, message: "Errand was not found." });
-        }
-    
-        if (errand.archived === false) {
-          errand.archived = true;
-          await errandRepository.update(errand);
-        } else {
-          return res
-            .status(StatusCodes.UNAUTHORIZED)
-            .send({ ok: false, message: "Errand already archived." });
-        }
-    
-        return res
-          .status(StatusCodes.OK)
-          .send({ ok: true, message: "Errand was successfully archived." });
+        const usecase = new archiveErrandUsecase();
+        const result = await usecase.execute({userId, errandId});
+
+        return res.status(StatusCodes.OK).send({
+          result
+        });
+
       } catch (error: any) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
           ok: false,
@@ -134,38 +113,13 @@ export class ErrandController {
       try {
         const { userId, errandId } = req.params;
     
-        const user = await new UserRepository().get(userId);
-        if (!user) {
-        return res
-          .status(StatusCodes.NOT_FOUND)
-          .send({ ok: false, message: "User was not found" });
-        }
+        const usecase = new unarchiveErrandUsecase();
+        const result = await usecase.execute({userId, errandId});
 
-    
-        const errandRepository = new ErrandRepository();
-        const errand = await new ErrandRepository().get({
-          errandId,
+        return res.status(StatusCodes.OK).send({
+          result
         });
-        
-        if (!errand) {
-          return res
-            .status(StatusCodes.NOT_FOUND)
-            .send({ ok: false, message: "Errand was not found." });
-        }
-        
-        if(errand._archived === true){
-          errand._archived = false;
-          await errandRepository.update(errand);
-        }
-        else{
-          return res
-            .status(StatusCodes.UNAUTHORIZED)
-            .send({ ok: false, message: "Errand already unarchived." });
-        }
     
-        return res
-          .status(StatusCodes.OK)
-          .send({ ok: true, message: "Errand was successfully unarchived." });
       } catch (error: any) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
           ok: false,
@@ -174,42 +128,4 @@ export class ErrandController {
       }
     }
     
-  public  async filterErrands(req: Request, res: Response) {
-  try {
-    const { userId } = req.params;
-    const { description, archived } = req.query;
-
-    const user = await new UserRepository().get(userId);
-        if (!user) {
-        return res
-          .status(StatusCodes.NOT_FOUND)
-          .send({ ok: false, message: "User was not found" });
-        }
-
-        let errands = await new ErrandRepository().list({
-            userId: userId,
-        });
-
-    if (description) {
-      const lowerCaseDescription = String(description).toLowerCase();
-      errands = errands.filter(
-        (errand) => errand.description.toLowerCase().includes(lowerCaseDescription)
-      );
-    }
-
-    
-    if (archived !== "") {
-      const isArchived = archived === 'true';
-      errands = errands.filter((errand) => errand._archived === isArchived);
-    }
-
-    return res.status(StatusCodes.OK).send({ ok: true, errands: errands });
-  } catch (error: any) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-      ok: false,
-      message: error.toString(),
-    });
-  }
 }
-};
-  
