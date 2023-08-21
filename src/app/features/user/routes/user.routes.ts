5222c345844f53e86242de8b792048ccb7da80cb
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-20 22:27:06 -0300

    feat: inseridos validdores nas rotas

--- src/app/features/user/routes/user.routes.ts
+++ src/app/features/user/routes/user.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 import { errandRoutes } from "../../errand/routes/errand.routes";
+import { UserValidator } from "../validators/user.validator";
 
 export const appRoutes = () => {
     const app = Router();
 
     app.get("/", new UserController().listUser);
-    app.post("/", new UserController().createUser);
-    app.put("/:userId", new UserController().updateUser);
+    app.post("/", UserValidator.createUserValidator, new UserController().createUser);
+    app.put("/:userId", UserValidator.updateUserValidator, new UserController().updateUser);
     app.delete("/:userId", new UserController().deleteUser);
-    app.post("/login", new UserController().login);
+    app.post("/login", UserValidator.loginValidator, new UserController().login);
 
     app.use("/:userId/errands", errandRoutes());
 
