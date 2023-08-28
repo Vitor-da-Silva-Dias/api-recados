e2d192ef2655acd6113569ca809a41c628d79b90
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-28 19:43:51 -0300

    feat: teste unitário do create-user-usecase

---
+++ tests/app/features/user/usecases/create-user.usecase.spec.ts
@@ -0,0 +1,60 @@
+import {createUserUsecase} from "../../../../../src/app/features/user/usecases/create-user.usecase";
+import {UserRepository} from "../../../../../src/app/features/user/repositories/user.repository";
+import { User } from "../../../../../src/app/models/user.model";
+import { CacheRepository } from "../../../../../src/app/shared/database/repositories/cache.repository";
+import { CacheDatabase } from "../../../../../src/main/database/cache.connection";
+import { Database } from "../../../../../src/main/database/database.connection";
+
+
+describe("Create User Usecase", () => {
+    beforeAll(async () => {
+      await Database.connect();
+      await CacheDatabase.connect();
+    });
+  
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+  
+    afterAll(async () => {
+      await Database.connection.destroy();
+      await CacheDatabase.connection.quit();
+    });
+
+    const userMockSut = () => {
+        return new User(
+          "any_name",
+          "any_email",
+          "any_password",
+        );
+      };
+
+    const createSut = () => {
+        return new createUserUsecase();
+    };
+
+    test("Retorna 201 caso o usuário seja criado com sucesso", async () => {
+        const user = userMockSut();
+        const sut = createSut();
+        
+        const result = await sut.execute({
+            name: "any_name",
+            email: "any_email",
+            password: "any_password"    
+        });
+
+        jest.spyOn(UserRepository.prototype, "create").mockResolvedValue(user);
+        jest.spyOn(CacheRepository.prototype, "setEx").mockResolvedValue();
+        jest.spyOn(CacheRepository.prototype, "delete").mockResolvedValue();
+
+        expect(result).toBeDefined();
+        expect(result.code).toBe(201);
+        expect(result.ok).toBe(true);
+        expect(result.message).toBe("User successfully created");
+        expect(result).toHaveProperty("data", result.data);
+    });
+
+    
+});
+
