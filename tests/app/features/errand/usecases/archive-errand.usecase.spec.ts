bc04781c507854b1a7b151fc143329100b5f635e
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-30 03:30:17 -0300

    feat: teste unitário para o archive-errand-usecase

---
+++ tests/app/features/errand/usecases/archive-errand.usecase.spec.ts
@@ -0,0 +1,117 @@
+import { archiveErrandUsecase } from "../../../../../src/app/features/errand/usecases/archive-errand-usecase";
+import { UserRepository } from "../../../../../src/app/features/user/repositories/user.repository";
+import { ErrandRepository } from "../../../../../src/app/features/errand/repositories/errand.repository";
+import { User } from "../../../../../src/app/models/user.model";
+import { CacheRepository } from "../../../../../src/app/shared/database/repositories/cache.repository";
+import { CacheDatabase } from "../../../../../src/main/database/cache.connection";
+import { Database } from "../../../../../src/main/database/database.connection";
+import { Errand } from "../../../../../src/app/models/errand.model";
+
+
+describe("Archive Errand Usecase", () => {
+    beforeAll(async () => {
+      await Database.connect();
+      await CacheDatabase.connect();
+    });
+  
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    const userMockSut = new User(
+        "any_name",
+        "any_email",
+        "any_password",
+    );
+
+    const errandMockSut = new Errand(
+        "any_description",
+        "any_detail",
+        userMockSut,
+        false
+    );
+
+    const createSut = () => {
+        return new archiveErrandUsecase();
+    };
+  
+    afterAll(async () => {
+      await Database.connection.destroy();
+      await CacheDatabase.connection.quit();
+    });
+
+    test("deveria retornar falha caso o usuário não seja encontrado", async () => {
+        const sut = createSut();
+
+        jest.spyOn(UserRepository.prototype, "get").mockResolvedValue(undefined);
+
+        const result = await sut.execute({
+            userId: "wrong_userId",
+            errandId: "any_errandId"
+        });
+
+        expect(result).toBeDefined();
+        expect(result.ok).toBe(false);
+        expect(result.code).toBe(404);
+        expect(result.message).toEqual("User not found");
+    });
+
+    test("deveria retornar falha caso o recado não seja encontrado", async () => {
+        const sut = createSut();
+
+        jest.spyOn(UserRepository.prototype, "get").mockResolvedValue(userMockSut);
+        jest.spyOn(ErrandRepository.prototype, "get").mockResolvedValue(undefined);
+
+        const result = await sut.execute({
+            errandId: "wrong_errandId",
+            userId: "any_userId"
+        });
+
+        expect(result).toBeDefined();
+        expect(result.ok).toBe(false);
+        expect(result.code).toBe(404);
+        expect(result.message).toEqual("Errand not found");
+    });
+
+    test("deveria retornar sucesso caso o recado seja arquivado", async () =>{
+        const sut = createSut();
+
+        jest.spyOn(UserRepository.prototype, "get").mockResolvedValue(userMockSut);
+        jest.spyOn(ErrandRepository.prototype, "get").mockResolvedValue(errandMockSut);
+
+        const result = await sut.execute({
+            errandId: "any_errandId",
+            userId: "any_userId",
+            archived: true
+        });
+
+        jest.spyOn(ErrandRepository.prototype, "update").mockResolvedValue();
+        jest.spyOn(CacheRepository.prototype, "delete").mockResolvedValue();
+
+        expect(result).toBeDefined();
+        expect(result.ok).toBe(true);
+        expect(result.code).toBe(200);
+        expect(result.message).toEqual("Errand successfully archived");
+        expect(result).toHaveProperty("data");
+    })
+
+    test("deveria retornar falha caso o recado já esteja arquivado", async () =>{
+        const sut = createSut();
+        const archived = errandMockSut.archived = true;
+
+        jest.spyOn(UserRepository.prototype, "get").mockResolvedValue(userMockSut);
+        jest.spyOn(ErrandRepository.prototype, "get").mockResolvedValue(errandMockSut);
+
+        const result = await sut.execute({
+            errandId: "any_errandId",
+            userId: "any_userId",
+            archived
+        });
+
+        expect(result).toBeDefined();
+        expect(result.ok).toBe(false);
+        expect(result.code).toBe(400);
+        expect(result.message).toEqual("Bad request: Errand already archived");
+    })
+});
